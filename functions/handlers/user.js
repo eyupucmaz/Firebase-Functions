f0f076be36cfc01e8fe754e281aa303bd2000fee
Eyup Ucmaz <eyupucmaz@gmail.com>
2020-12-02 15:00:48 +0300

    created user file for the user routing

---
+++ functions/handlers/user.js
@@ -0,0 +1,95 @@
+const { db } = require("../util/admin");
+const firebase = require("firebase");
+const config = require("../util/config");
+const {validateSignUpData, validateLoginData} = require('../util/validators')
+
+firebase.initializeApp(config);
+
+
+
+exports.signUp = (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword,
+		handle: req.body.handle,
+   };
+   
+   const {valid, errors} =validateSignUpData(newUser);
+   if(!valid) return res.status(400).json(errors);
+	
+	//* if we do not have any errors
+	let token, userId;
+	db.doc(`/users/${newUser.handle}`)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				return res.status(400).json({ handle: "this handle is already taken" });
+			} else {
+				return firebase
+					.auth()
+					.createUserWithEmailAndPassword(newUser.email, newUser.password);
+			}
+		})
+		.then((data) => {
+			userId = data.user.uid;
+			return data.user.getIdToken();
+		})
+		.then((userToken) => {
+			token = userToken;
+			const userCredentials = {
+				handle: newUser.handle,
+				email: newUser.email,
+				createdAt: new Date().toISOString(),
+				userId,
+			};
+			db.doc(`/users/${newUser.handle}`).set(userCredentials);
+		})
+		.then(() => {
+			return res.status(201).json({ token });
+		})
+		.catch((err) => {
+			console.error(err);
+			if (err.code === "auth/email-already-in-use") {
+				return res.status(400).json({ email: "Email is already in use" });
+			} else {
+				return res.status(500).json({ error: err.code });
+			}
+		});
+};
+
+exports.login = (req, res) => {
+	const user = {
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+   //Data Validation
+   const {valid, errors} =validateLoginData(user);
+   if(!valid) return res.status(400).json(errors);
+
+	//! if we have any errors
+
+	
+
+	//* if we do not have any errors
+	firebase
+		.auth()
+		.signInWithEmailAndPassword(user.email, user.password)
+		.then((data) => {
+			return data.user.getIdToken();
+		})
+		.then((token) => {
+			return res.json({ token });
+		})
+		.catch((err) => {
+			console.error(err);
+			if (err.code === "auth/wrong-password") {
+				return res
+					.status(403)
+					.json({ general: "Wrong credentials, please try again" });
+			} else {
+				return res.status(500).json({ error: err.code });
+			}
+		});
+};
