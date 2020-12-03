94863126a7b0798e8b8c4d5dcf0d30a40b039950
Eyup Ucmaz <eyupucmaz@gmail.com>
2020-12-03 14:18:13 +0300

    Created user image route

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signUp, login } = require("./handlers/user");
+const { signUp, login, uploadImage } = require("./handlers/user");
 const FBAuth = require("./util/fbAuth");
 
-
-
+//! SCREAM ROUTES
 //Get all posts - scream route
 app.get("/screams", getAllScreams);
 //Create a post
 app.post("/scream", FBAuth, postOneScream);
 
-
+//! USER ROUTES
 //Singup Route
 app.post("/signup", signUp);
 //Login Route
 app.post("/login", login);
+//Image Upload
+app.post("/user/image", FBAuth, uploadImage);
 
 // Exporting express request, it should looks like that https://baseurl.com/api/${OUR_REQUESTS}
 exports.api = functions.region("europe-west1").https.onRequest(app);
