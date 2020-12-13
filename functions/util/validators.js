760e328cc5e5a4084764c58b67e90abda3659d10
Eyup Ucmaz <eyupucmaz@gmail.com>
2020-12-13 19:04:14 +0300

    Valid the user details

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,3 +1,5 @@
+const { user } = require("firebase-functions/lib/providers/auth");
+
 // Validation helper functions
 const isEmpty = (str) => {
 	if (str.trim() === "") return true;
@@ -39,4 +41,17 @@ exports.validateLoginData = (data) => {
 		errors,
 		valid: Object.keys(errors).length === 0 ? true : false,
 	};
+};
+
+exports.reduceUserDetails = (data) => {
+	let userDetails = {};
+	if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+	if (!isEmpty(data.website.trim())) {
+		if (data.website.trim().substring(0, 4) !== "http") {
+			userDetails.website = `http://${data.website.trim()}`;
+		} else userDetails = data.website;
+	}
+	if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+	return userDetails;
 };
