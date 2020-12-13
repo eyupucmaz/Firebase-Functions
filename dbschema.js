3e7c314a9ef53913c15d90bc0eba29740e17d8a9
Eyup Ucmaz <eyupucmaz@gmail.com>
2020-12-13 19:05:14 +0300

    Create a user schema

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+	users: [
+		{
+			userId: "kasdlf098a345ajkdhfa9p38q",
+			email: "user@email.com",
+			handle: "user",
+			createdAt: "2020-03-12T10:45:34.123Z",
+			imageUrl: "image/myprifle/photo",
+			bio: "Hello 👋 I am eyup",
+			website: "eyupucmaz.site",
+			location: "World",
+		},
+	],
 	screams: [
 		{
 			userHandle: "user",
